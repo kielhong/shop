784286d86a371c43d35bef09903fadab0b3cbc5e
kielhong <kielhong@naver.com>
2016-12-14 17:20:17 -0500

    OrderView DTO 조회 쿼리

--- src/main/java/com/widehouse/repository/OrderRepository.java
+++ src/main/java/com/widehouse/repository/OrderRepository.java
@@ -2,11 +2,22 @@ package com.widehouse.repository;
 
 
 import com.widehouse.domain.order.Order;
+import com.widehouse.dto.OrderView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by kiel on 2016. 6. 17..
  */
 public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
+
+    @Query("SELECT new com.widehouse.dto.OrderView(o, m) " +
+            "FROM Order o INNER JOIN o.orderer m " +
+            "WHERE m.memberId.id = :ordererId " +
+            "ORDER BY o.id DESC")
+    List<OrderView> findByOrdererId(@Param("ordererId") String ordererId);
 }
--- src/test/java/com/widehouse/repository/OrderRepositoryTest.java
+++ src/test/java/com/widehouse/repository/OrderRepositoryTest.java
@@ -1,22 +1,24 @@
 package com.widehouse.repository;
 
-import static com.widehouse.repository.OrderSpecifications.between;
 import static com.widehouse.repository.OrderSpecifications.isOrderByMember;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.widehouse.domain.member.Member;
+import com.widehouse.domain.member.MemberId;
 import com.widehouse.domain.order.Order;
 import com.widehouse.domain.order.OrderLine;
 import com.widehouse.domain.order.Receiver;
 import com.widehouse.domain.order.ShippingAddress;
 import com.widehouse.domain.order.ShippingInfo;
 import com.widehouse.domain.product.Product;
+import com.widehouse.dto.OrderView;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
@@ -36,40 +38,67 @@ public class OrderRepositoryTest {
     @Autowired
     private OrderRepository repository;
 
+    private Product product;
+    private List<OrderLine> orderLines;
+    private Receiver receiver;
+    private ShippingAddress shippingAddress;
+    private ShippingInfo shippingInfo;
+    @Before
+    public void setUp() {
+        product = new Product();
+        orderLines = Arrays.asList(new OrderLine(product, 100, 1));
+        receiver = new Receiver("tester", "123-456-7890");
+        shippingAddress = new ShippingAddress("", "", "Reston", "");
+        shippingInfo = new ShippingInfo(receiver, shippingAddress);
+    }
+
     @Test
-    public void shouldFindOrderByOrdererId() {
+    public void shouldFindOrdersByOrdererId() {
         // Given
-        Product product = new Product();
-        List<OrderLine> orderLines = Arrays.asList(new OrderLine(product, 100, 1));
-        String ordererId1 = "orderer";
-        String ordererId2 = "orderer2";
-        Receiver receiver = new Receiver("tester", "123-456-7890");
-        ShippingAddress shippingAddress = new ShippingAddress("", "", "Reston", "");
-        ShippingInfo shippingInfo = new ShippingInfo(receiver, shippingAddress);
-        entityManager.persist(new Order(orderLines, ordererId1, LocalDateTime.now(), shippingInfo));
-        entityManager.persist(new Order(orderLines, ordererId1, LocalDateTime.now(), shippingInfo));
-        entityManager.persist(new Order(orderLines, ordererId2, LocalDateTime.now(), shippingInfo));
+        Member orderer1 = new Member(new MemberId("orderer1"), "orderder1");
+        Member orderer2 = new Member(new MemberId("orderer2"), "orderder2");
+        entityManager.persist(orderer1);
+        entityManager.persist(orderer2);
+        entityManager.persist(new Order(orderLines, orderer1, LocalDateTime.now(), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer1, LocalDateTime.now(), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer2, LocalDateTime.now(), shippingInfo));
 
         // When
-        List<Order> results = repository.findAll(isOrderByMember(ordererId1));
+        List<Order> results = repository.findAll(isOrderByMember(orderer1));
         // Then
         assertThat(results.size()).isEqualTo(2);
+        results.stream().allMatch(x -> x.getOrderer().equals(orderer1));
+    }
+
+    @Test
+    public void shouldFindOrderViewsByOrdererId() {
+        // Given
+        String orderedId1 = "orderer1";
+        Member orderer1 = new Member(new MemberId(orderedId1), "orderder1");
+        Member orderer2 = new Member(new MemberId("orderer2"), "orderder2");
+        entityManager.persist(orderer1);
+        entityManager.persist(orderer2);
+        entityManager.persist(new Order(orderLines, orderer1, LocalDateTime.now(), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer1, LocalDateTime.now(), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer2, LocalDateTime.now(), shippingInfo));
+
+        // When
+        List<OrderView> results = repository.findByOrdererId(orderedId1);
+        // Then
+        assertThat(results.size()).isEqualTo(2);
+        results.stream().allMatch(x -> x.getOrdererId().equals(orderedId1));
     }
 
     @Test
     public void shouldFindBetweenDate() {
         // Given
-        Product product = new Product();
-        List<OrderLine> orderLines = Arrays.asList(new OrderLine(product, 100, 1));
-        String ordererId = "orderer";
-        Receiver receiver = new Receiver("tester", "123-456-7890");
-        ShippingAddress shippingAddress = new ShippingAddress("", "", "Reston", "");
-        ShippingInfo shippingInfo = new ShippingInfo(receiver, shippingAddress);
-        entityManager.persist(new Order(orderLines, ordererId, LocalDateTime.now(), shippingInfo));
-        entityManager.persist(new Order(orderLines, ordererId, LocalDateTime.now().minusDays(1), shippingInfo));
-        entityManager.persist(new Order(orderLines, ordererId, LocalDateTime.now().minusDays(2), shippingInfo));
-        entityManager.persist(new Order(orderLines, ordererId, LocalDateTime.now().minusDays(3), shippingInfo));
-        entityManager.persist(new Order(orderLines, ordererId, LocalDateTime.now().minusDays(10), shippingInfo));
+        Member orderer = new Member(new MemberId("orderer1"), "orderder1");
+        entityManager.persist(orderer);
+        entityManager.persist(new Order(orderLines, orderer, LocalDateTime.now(), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer, LocalDateTime.now().minusDays(1), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer, LocalDateTime.now().minusDays(2), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer, LocalDateTime.now().minusDays(3), shippingInfo));
+        entityManager.persist(new Order(orderLines, orderer, LocalDateTime.now().minusDays(10), shippingInfo));
         // When
         LocalDate now = LocalDate.now();
         LocalDateTime to = now.atTime(23, 59, 59);
